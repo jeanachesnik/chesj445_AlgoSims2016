8f1a283eb7578cd818b4feac5a4fd4269845e620
jeanachesnik <chesj445@newschool.edu>
2016-12-09 01:01:30 -0500

    Add files via upload

---
+++ finalprototype6/src/Square.hpp
@@ -0,0 +1,14 @@
+//
+//  Square.hpp
+//  finalprototype6
+//
+//  Created by Jeana Chesnik on 12/1/16.
+//
+//
+
+#ifndef Square_hpp
+#define Square_hpp
+
+#include <stdio.h>
+
+#endif /* Square_hpp */
